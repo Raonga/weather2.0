5cb48fcd0bc8e4f1de8a30cf4c272343d284b6d2
Raonga <uanraonga@gmail.com>
2023-06-25 20:51:11 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -67,9 +67,9 @@ function displayForecast(response) {
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
-function getForecast(coordinates) {
+function getForecast(response) {
   let apiKey = "0f9184c6bbbd99ef0f03atcoa48342a8";
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.lon}&lat=${coordinates.lat}&key=${apikey}&units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${lon}&lat=${lat}&key=${apikey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -93,7 +93,7 @@ function showTemperature(response) {
   document.querySelector("#date").innerHTML = formatDate(
     response.data.time * 1000
   );
-  getForecast(response.data.coord);
+  getForecast(response.data.forecast);
 }
 
 function displayCity(city) {
