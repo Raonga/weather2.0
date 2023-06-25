40942cfe2828075b37478e834d8e28d9864477a3
Raonga <uanraonga@gmail.com>
2023-06-25 21:53:27 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -69,7 +69,7 @@ function displayForecast(response) {
 }
 function getForecast(coordinates) {
   let apiKey = "743bee57fddbfaf52447193a87d5dd25";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.latitude}&lon=${coordinates.longitude}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
